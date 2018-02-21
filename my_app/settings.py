5ea66a66e93b1a0c7bc45bd3c81b7921e32c5bd2
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-02-21 19:18:35 +0100

    fixed index.html viewport

--- my_app/settings.py
+++ my_app/settings.py
@@ -131,4 +131,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
