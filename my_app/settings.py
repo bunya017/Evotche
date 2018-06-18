3103a84d051698344c82020b090bf1205d5d3af1
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-06-18 21:41:56 +0100

    update settings and requirements.txt

--- my_app/settings.py
+++ my_app/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['evotche.herokuapp.com', '192.168.43.33', '127.0.0.1', 'localho
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
     'users.apps.UsersConfig',
+    'transactions.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
