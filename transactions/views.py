d169a328d94c3fc9d3a6159a4d5dceff25112be2
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-09-17 17:19:54 +0100

    Removed text & email delivery from free & paid token purchase view and template

--- transactions/views.py
+++ transactions/views.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import redirect
-from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.shortcuts import get_object_or_404, get_list_or_404
+from django.core.urlresolvers import reverse
+from django.db.models import Q
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, get_list_or_404, redirect, render
 import datetime
 from my_app.settings import PAYANT_AUTH_KEY as key
-from pypayant import Client, Invoice, Payment
-from polls.models import BallotPaper
-from users.models import Profile, Token
 from .models import PurchaseInvoice, Item
 from .forms import InvoiceForm, FreeTokenForm
 from .snippets import auth_payant, gen_token, make_dict, makeRefCode
 from polls.snippets import check_usable_password
+from polls.models import BallotPaper
+from users.models import Profile, Token
+from pypayant import Client, Invoice, Payment
 from requests import ConnectionError
 
 
@@ -72,10 +71,9 @@ def buy_tokens(request, ballot_url):
 				update_profile = Profile(user=request.user, phone=cl_form['phone'])
 				update_profile.save()
 			try:
-				iv = PurchaseInvoice.objects.get(
-					ballot_paper=ballot,
-					ballot_paper__has_free_tokens=False,
-					ballot_paper__has_paid_tokens=False
+				PurchaseInvoice.objects.get(
+					Q(ballot_paper=ballot),
+					Q(ballot_paper__has_free_tokens=True) | Q(ballot_paper__has_paid_tokens=True)
 				)
 			except (PurchaseInvoice.DoesNotExist):
 				p_invoice = PurchaseInvoice.objects.create(
@@ -162,142 +160,38 @@ def get_free_tokens(request, ballot_url):
 			if cl_form['phone']:
 				update_profile = Profile(user=request.user, phone=cl_form['phone'])
 				update_profile.save()
-			if cl_form['quantity'] > 30:
-				context2 = {'form': form, 'ballot': ballot, 'above30': 'Sorry, you cannot get more than 30 FREE tokens.'}
+			if cl_form['quantity'] > 20:
+				context2 = {'form': form, 'ballot': ballot, 'above20': 'Sorry, you cannot get more than 20 FREE tokens.'}
 				return render(request, 'transactions/free_tokens.html', context2)
 			elif cl_form['quantity'] < 1:
 				context21 = {'form': form, 'ballot': ballot, 'zero': 'Please ensure this value is greater than 0.'}
 				return render(request, 'transactions/free_tokens.html', context21)
 			try:
-				iv = PurchaseInvoice.objects.get(
-					ballot_paper=ballot,
-					ballot_paper__has_free_tokens=True,
-					ballot_paper__has_paid_tokens=False
+				PurchaseInvoice.objects.get(
+					Q(ballot_paper=ballot),
+					Q(ballot_paper__has_free_tokens=True) | Q(ballot_paper__has_paid_tokens=True)
 				)
 			except (PurchaseInvoice.DoesNotExist):
-				if (cl_form['email_delivery'] == False) and (cl_form['text_delivery'] == False):
-					f_invoice = PurchaseInvoice(
-						user=request.user,
-						ballot_paper=ballot,
-						reference_code=makeRefCode(str('free ' + ballot.ballot_name)),
-						date_created=datetime.datetime.now(),
-						due_date=datetime.datetime.now().date() + datetime.timedelta(days=2)
-					)
-					f_invoice.status = 'successful'
-					f_invoice.save()
-					token_item = Item.objects.create(
-						invoice=f_invoice,
-						item='Voter Token',
-						description='Free Voter Tokens for %s.' % (ballot.ballot_name),
-						unit_cost=0.00,
-						quantity=cl_form['quantity']
-					)
-					token_item.save()
-					ballot.has_free_tokens = True
-					ballot.save()
-					return HttpResponseRedirect(reverse('trxns:get_invoice', args=[f_invoice.reference_code]))
-				else:
-					f_invoice = PurchaseInvoice(
-						user=request.user,
-						ballot_paper=ballot,
-						date_created=datetime.datetime.now(),
-						due_date=datetime.datetime.now().date() + datetime.timedelta(days=2)
-					)
-					f_invoice.save()
-					token_item = Item.objects.create(
-						invoice=f_invoice,
-						item='Voter Token',
-						description='Free Voter Tokens for %s.' % (ballot.ballot_name),
-						unit_cost=0.00,
-						quantity=cl_form['quantity']
-					)
-					token_item.save()
-					token_item_dict = make_dict(token_item)
-					item_list=[token_item_dict]
-
-					if (cl_form['email_delivery'] == True) and (cl_form['text_delivery'] == True):
-						email_item = Item.objects.create(
-							invoice=f_invoice,
-							item='Email Delivery',
-							description='Delivery of Voter Tokens via email.',
-							unit_cost=1.00,
-							quantity=cl_form['quantity']
-						)
-						email_item.save()
-						email_item_dict = make_dict(email_item)
-
-						text_item = Item.objects.create(
-							invoice=f_invoice,
-							item='Text Delivery',
-							description='Delivery of Voter Tokens via text.',
-							unit_cost=5.00,
-							quantity=cl_form['quantity']
-						)
-						text_item.save()
-						text_item_dict = make_dict(text_item)
-						item_list=[token_item_dict, email_item_dict, text_item_dict]
-					elif (cl_form['email_delivery'] == True) and (cl_form['text_delivery'] == False):
-						email_item = Item.objects.create(
-							invoice=f_invoice,
-							item='Email Delivery',
-							description='Delivery of Voter Tokens via email.',
-							unit_cost=1.00,
-							quantity=cl_form['quantity']
-						)
-						email_item.save()
-						email_item_dict = make_dict(email_item)
-						item_list=[token_item_dict, email_item_dict]
-					elif (cl_form['text_delivery'] == True) and (cl_form['email_delivery'] == False):
-						text_item = Item.objects.create(
-							invoice=f_invoice,
-							item='Text Delivery',
-							description='Delivery of Voter Tokens via text.',
-							unit_cost=5.00,
-							quantity=cl_form['quantity']
-						)
-						text_item.save()
-						text_item_dict = make_dict(text_item)
-						item_list=[token_item_dict, text_item_dict]
-
-					try:
-						auth_payant(key, 'demo')
-					except (ConnectionError):
-						f_invoice.delete()
-						context3 = {'form': form, 'ballot': ballot, 'conn_error': 'Network connection error, try again.'}
-						return render(request, 'transactions/free_tokens.html', context3)
-					else:
-						pay_invoice = Invoice(key)
-						if request.user.profile.payant_id:
-							new_invoice = pay_invoice.add(
-								client_id=(request.user.profile.payant_id),
-								due_date=f_invoice.due_date.strftime('%m/%d/%Y'),
-								fee_bearer='account',
-								items=item_list
-							)
-						else:
-							new_client = {
-								"first_name": request.user.first_name,
-								"last_name": request.user.last_name,
-								"email": request.user.email,
-								"phone": request.user.profile.phone
-							}
-							new_invoice = pay_invoice.add(
-								new=True,
-								client=new_client,
-								due_date=f_invoice.due_date.strftime('%m/%d/%Y'),
-								fee_bearer='account',
-								items=item_list
-							)
-						f_invoice.reference_code = new_invoice[2]['reference_code']
-						f_invoice.status = new_invoice[2]['status']
-						f_invoice.save()
-						if not request.user.profile.payant_id:
-							request.user.profile.payant_id = new_invoice[2]['client_id']
-							request.user.profile.save()
-						return HttpResponseRedirect(reverse(
-							'trxns:get_invoice',
-							args=[f_invoice.reference_code])
-						)
+				f_invoice = PurchaseInvoice(
+					user=request.user,
+					ballot_paper=ballot,
+					reference_code=makeRefCode(str('free ' + ballot.ballot_name)),
+					date_created=datetime.datetime.now(),
+					due_date=datetime.datetime.now().date() + datetime.timedelta(days=2)
+				)
+				f_invoice.status = 'successful'
+				f_invoice.save()
+				token_item = Item.objects.create(
+					invoice=f_invoice,
+					item='Voter Token',
+					description='Free Voter Tokens for %s.' % (ballot.ballot_name),
+					unit_cost=0.00,
+					quantity=cl_form['quantity']
+				)
+				token_item.save()
+				ballot.has_free_tokens = True
+				ballot.save()
+				return HttpResponseRedirect(reverse('trxns:get_invoice', args=[f_invoice.reference_code]))
 			else:
 				context = {'form': form,
 					'ballot': ballot,
